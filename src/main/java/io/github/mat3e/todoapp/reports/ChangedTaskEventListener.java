2f5affe8651aa31b21534076c9f25939a5aa46d2
Marcin Milczek <MarcinMilczek@simple.com.pl>
2022-07-11 07:50:19 +0200

    update kodu

--- src/main/java/io/github/mat3e/todoapp/reports/ChangedTaskEventListener.java
+++ src/main/java/io/github/mat3e/todoapp/reports/ChangedTaskEventListener.java
@@ -1,10 +1,12 @@
 package io.github.mat3e.todoapp.reports;
 
 import io.github.mat3e.todoapp.model.event.TaskDone;
+import io.github.mat3e.todoapp.model.event.TaskEvent;
 import io.github.mat3e.todoapp.model.event.TaskUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,17 +20,20 @@ public class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     public void on(TaskDone event) {
         onChanged(event);
     }
 
-    private void onChanged(TaskDone event) {
+    @Async
+    @EventListener
+    public void on(TaskUndone event) {
         onChanged(event);
     }
 
-    @EventListener
-    public void on(TaskUndone event) {
+    public void  onChanged(final TaskEvent event) {
         logger.info("Got " + event);
+        repository.save(new PersistedTaskEvent(event));
     }
 }
--- src/main/resources/db/migration/V9__init_task_events_table.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists tasks_events;
-create table tasks_events
-(
-    id          int primary key auto_increment,
-    task_id     int ,
-    occurence   datetime,
-    name        varchar(30)
-)
